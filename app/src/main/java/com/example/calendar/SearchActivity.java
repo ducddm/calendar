5a637cf7f05663afeced63d3c394d0988ca8915d
ducddm <ducddm.b8042@st.usth.edu.vn>
2020-02-03 14:48:41 +0700

    Update Solar Fragment

--- app/src/main/java/com/example/calendar/SearchActivity.java
+++ app/src/main/java/com/example/calendar/SearchActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -56,6 +57,240 @@ public class SearchActivity extends AppCompatActivity {
         int d[]=calcu.Solar2Lunar(da,m,y);
         solar.setText(da+"/"+m+"/"+y);
         lunar.setText(d[0]+"/"+d[1]+"/"+d[2]);
+
+        TextView day1 = findViewById(R.id.thu);
+        int X=calcu.MOD((int)(Calculation.UniversalToJD(da,m,y)+2.5), 7);
+        switch (X){
+            case 0: day1.setText(R.string.sat); break;
+            case 1: day1.setText(R.string.sun); break;
+            case 2: day1.setText(R.string.mon); break;
+            case 3: day1.setText(R.string.tue); break;
+            case 4: day1.setText(R.string.wed); break;
+            case 5: day1.setText(R.string.thu); break;
+            case 6: day1.setText(R.string.fri); break;
+        }
+
+        /*Cung Hoàng Đạo*/
+
+        ImageView star = findViewById(R.id.starr);
+        TextView st = findViewById(R.id.sta);
+        TextView ele = findViewById(R.id.ele);
+        TextView pla = findViewById(R.id.pla);
+        TextView pla1 = findViewById(R.id.pla1);
+        TextView ang = findViewById(R.id.ang);
+        TextView ang1 = findViewById(R.id.ang1);
+
+        switch (m) {
+            case 1: if (da<20) {
+                star.setImageResource(R.drawable.capricorn);
+                st.setText(R.string.s1);
+                ele.setText(R.string.dat);
+                pla.setText(R.string.st8);
+                ang.setText(R.string.a11);
+            }
+            else {
+                star.setImageResource(R.drawable.aquarius);
+                st.setText(R.string.s2);
+                ele.setText(R.string.k);
+                pla.setText(R.string.st8);
+                pla1.setText(R.string.st9);
+                ang.setText(R.string.a11);
+                ang1.setText(R.string.a12);
+            }
+                break;
+
+            case 2: if (da<19) {
+                star.setImageResource(R.drawable.aquarius);
+                st.setText(R.string.s2);
+                ele.setText(R.string.k);
+                pla.setText(R.string.st8);
+                pla1.setText(R.string.st9);
+                ang.setText(R.string.a11);
+                ang1.setText(R.string.a12);
+            }
+            else {
+                star.setImageResource(R.drawable.pisces);
+                st.setText(R.string.s3);
+                ele.setText(R.string.n);
+                pla.setText(R.string.st10);
+                ang.setText(R.string.a13);
+            }
+                break;
+
+            case 3: if (da<21) {
+                star.setImageResource(R.drawable.pisces);
+                st.setText(R.string.s3);
+                ele.setText(R.string.n);
+                pla.setText(R.string.st10);
+                ang.setText(R.string.a13);
+
+            }
+            else {
+                star.setImageResource(R.drawable.aries);
+                st.setText(R.string.s4);
+                ele.setText(R.string.l);
+                pla.setText(R.string.st1);
+                ang.setText(R.string.a1);
+            }
+                break;
+
+            case 4: if (da<20) {
+                star.setImageResource(R.drawable.aries);
+                st.setText(R.string.s4);
+                ele.setText(R.string.l);
+                pla.setText(R.string.st1);
+                ang.setText(R.string.a1);
+            }
+            else {
+                star.setImageResource(R.drawable.taurus);
+                st.setText(R.string.s5);
+                ele.setText(R.string.dat);
+                pla.setText(R.string.st2);
+                ang.setText(R.string.a2);
+            }
+                break;
+
+            case 5: if (da<21) {
+                star.setImageResource(R.drawable.taurus);
+                st.setText(R.string.s5);
+                ele.setText(R.string.dat);
+                pla.setText(R.string.st2);
+                ang.setText(R.string.a2);
+            }
+            else {
+                star.setImageResource(R.drawable.gemini);
+                st.setText(R.string.s6);
+                ele.setText(R.string.k);
+                pla.setText(R.string.st3);
+                ang.setText(R.string.a3);
+            }
+                break;
+
+            case 6: if (da<22) {
+                star.setImageResource(R.drawable.gemini);
+                st.setText(R.string.s6);
+                ele.setText(R.string.k);
+                pla.setText(R.string.st3);
+                ang.setText(R.string.a3);
+            }
+            else {
+                star.setImageResource(R.drawable.cancer);
+                st.setText(R.string.s7);
+                ele.setText(R.string.n);
+                pla.setText(R.string.st4);
+                ang.setText(R.string.a4);
+                ang1.setText(R.string.a5);
+            }
+                break;
+
+            case 7: if (da<23) {
+                star.setImageResource(R.drawable.cancer);
+                st.setText(R.string.s7);
+                ele.setText(R.string.n);
+                pla.setText(R.string.st4);
+                ang.setText(R.string.a4);
+                ang1.setText(R.string.a5);
+            }
+            else {
+                star.setImageResource(R.drawable.leo);
+                st.setText(R.string.s8);
+                ele.setText(R.string.l);
+                pla.setText(R.string.st5);
+                ang.setText(R.string.a6);
+            }
+                break;
+
+            case 8: if (da<23) {
+                star.setImageResource(R.drawable.leo);
+                st.setText(R.string.s8);
+                ele.setText(R.string.l);
+                pla.setText(R.string.st5);
+                ang.setText(R.string.a6);
+            }
+            else {
+                star.setImageResource(R.drawable.virgo);
+                st.setText(R.string.s9);
+                ele.setText(R.string.dat);
+                pla.setText(R.string.st3);
+                ang.setText(R.string.a3);
+                ang.setText(R.string.a5);
+            }
+                break;
+
+            case 9: if (da<23) {
+                star.setImageResource(R.drawable.virgo);
+                st.setText(R.string.s9);
+                ele.setText(R.string.dat);
+                pla.setText(R.string.st3);
+                ang.setText(R.string.a3);
+                ang.setText(R.string.a5);
+            }
+            else {
+                star.setImageResource(R.drawable.libra);
+                st.setText(R.string.s10);
+                ele.setText(R.string.k);
+                pla.setText(R.string.st2);
+                ang.setText(R.string.a7);
+                ang1.setText(R.string.a2);
+            }
+                break;
+
+            case 10: if (da<24) {
+                star.setImageResource(R.drawable.libra);
+                st.setText(R.string.s10);
+                ele.setText(R.string.k);
+                pla.setText(R.string.st2);
+                ang.setText(R.string.a7);
+                ang1.setText(R.string.a2);
+            }
+            else {
+                star.setImageResource(R.drawable.scorpio);
+                st.setText(R.string.s11);
+                ele.setText(R.string.n);
+                pla.setText(R.string.st6);
+                ang.setText(R.string.a8);
+                ang1.setText(R.string.a1);
+            }
+                break;
+
+            case 11: if (da<22) {
+                star.setImageResource(R.drawable.scorpio);
+                st.setText(R.string.s11);
+                ele.setText(R.string.n);
+                pla.setText(R.string.st6);
+                ang.setText(R.string.a8);
+                ang1.setText(R.string.a1);
+            }
+            else {
+                star.setImageResource(R.drawable.sagittarius);
+                st.setText(R.string.s12);
+                ele.setText(R.string.l);
+                pla.setText(R.string.st7);
+                ang.setText(R.string.a9);
+                ang1.setText(R.string.a10);
+            }
+                break;
+
+            case 12: if (da<22) {
+                star.setImageResource(R.drawable.sagittarius);
+                st.setText(R.string.s12);
+                ele.setText(R.string.l);
+                pla.setText(R.string.st7);
+                ang.setText(R.string.a9);
+                ang1.setText(R.string.a10);
+            }
+            else {
+                star.setImageResource(R.drawable.capricorn);
+                st.setText(R.string.s1);
+                ele.setText(R.string.dat);
+                pla.setText(R.string.st8);
+                ang.setText(R.string.a11);
+
+            }
+                break;
+        }
+
+
 
     }
 }
